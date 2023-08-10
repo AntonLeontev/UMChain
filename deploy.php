ff8f49bb9a67ff4362a8c17175dffd07dbacd499
Anton <aner-anton@yandex.ru>
2023-08-10 16:44:28 +0300

    composer

---
+++ deploy.php
@@ -0,0 +1,29 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/AntonLeontev/UMChain.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('45.146.165.254')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/umchain');
+
+task('build', function () {
+    cd('{{release_path}}');
+    run('npm install');
+    run('npm run build');
+});
+
+// Hooks
+after('deploy:publish', 'build');
+
+after('deploy:failed', 'deploy:unlock');
