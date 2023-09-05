77c9b629de343a50efb92cd81340f22190c30194
Anton <aner-anton@yandex.ru>
2023-09-05 18:29:53 +0300

    Повторные переходы не учитываются в статистике рефералов

--- app/Http/Middleware/DetectReferralLink.php
+++ app/Http/Middleware/DetectReferralLink.php
@@ -21,6 +21,10 @@ class DetectReferralLink
 			return $next($request);
 		}
 
+		if (session('referral') === $request->get('ref')) {
+			return $next($request);
+		}
+
 		if (ReferralLink::query()->where('id', $request->get('ref'))->exists()) {
 			ReferralLinkClick::create(['referral_link_id' => $request->get('ref')]);
 		}
