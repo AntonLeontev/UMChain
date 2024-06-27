8dd5e3eb8cd5889c52aad578fda9cc7a40c8ebcc
Anton <aner-anton@yandex.ru>
2024-06-27 10:51:06 +0300

    fix: video on ios

--- resources/js/Components/Quiz/PersonImage.vue
+++ resources/js/Components/Quiz/PersonImage.vue
@@ -4,56 +4,82 @@ import { onActivated, ref } from "vue";
 const dirName = ref(null);
 const ageDir = ref(null);
 let fatLevel = (+sessionStorage.getItem("quiz.fat_level") ?? 1) + 1;
-const fileName = ref(fatLevel + ".webm");
+const fileName = ref(fatLevel);
 
 onActivated(() => {
-    dirName.value = sessionStorage.getItem("quiz.sex") ?? "male";
-    const bday = sessionStorage.getItem("quiz.bday");
-    let age = calculateAge(bday);
+  dirName.value = sessionStorage.getItem("quiz.sex") ?? "male";
+  const bday = sessionStorage.getItem("quiz.bday");
+  let age = calculateAge(bday);
 
-    ageDir.value = age < 30 ? "18" : age < 40 ? "30" : age < 50 ? "40" : "50";
+  ageDir.value = age < 30 ? "18" : age < 40 ? "30" : age < 50 ? "40" : "50";
 });
 
 function calculateAge(birthDate) {
-    const birthDateParts = birthDate.split("-");
-    const birthYear = parseInt(birthDateParts);
-    const birthMonth = parseInt(birthDateParts);
-    const birthDay = parseInt(birthDateParts);
+  const birthDateParts = birthDate.split("-");
+  const birthYear = parseInt(birthDateParts);
+  const birthMonth = parseInt(birthDateParts);
+  const birthDay = parseInt(birthDateParts);
 
-    const today = new Date();
-    const currentYear = today.getFullYear();
-    const currentMonth = today.getMonth() + 1;
-    const currentDay = today.getDate();
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  const currentMonth = today.getMonth() + 1;
+  const currentDay = today.getDate();
 
-    let age = currentYear - birthYear;
+  let age = currentYear - birthYear;
 
-    if (
-        currentMonth < birthMonth ||
-        (currentMonth === birthMonth && currentDay < birthDay)
-    ) {
-        age--;
-    }
+  if (
+    currentMonth < birthMonth ||
+    (currentMonth === birthMonth && currentDay < birthDay)
+  ) {
+    age--;
+  }
 
-    return age;
+  return age;
 }
 </script>
 
 <template>
-    <div class="video_wrapper">
-        <video
-            autoplay
-            muted
-            loop
-            :src="`/video/${dirName}/${ageDir}/${fileName}`"
-			poster="/resources/images/video-poster.jpg"
-        />
-    </div>
+  <div class="video_wrapper">
+    <video
+      autoplay
+      muted
+      loop
+      disablepictureinpicture=""
+      webkit-playsinline=""
+      playsinline=""
+      pip="false"
+      :src="`/video/${dirName}/${ageDir}/${fileName}.webm`"
+      poster="/resources/images/video-poster.jpg"
+    />
+  </div>
 </template>
 
 <style scoped>
 .video_wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+video {
+  background-color: transparent;
+  border-bottom: 1px solid transparent; /* хак для предотвращения мелькания при загрузке на некоторых версиях ios */
+  &::-webkit-media-controls-panel {
+    display: none !important;
+    -webkit-appearance: none;
+    opacity: 0;
+    visibility: hidden;
+  }
+  &::-webkit-media-controls-play-button {
+    display: none !important;
+    -webkit-appearance: none;
+    opacity: 0;
+    visibility: hidden;
+  }
+  &::-webkit-media-controls-start-playback-button {
+    display: none !important;
+    -webkit-appearance: none;
+    opacity: 0;
+    visibility: hidden;
+  }
 }
 </style>
--- resources/js/Components/Quiz/Slides/FatLevel.vue
+++ resources/js/Components/Quiz/Slides/FatLevel.vue
@@ -12,149 +12,119 @@ const level = ref(sessionStorage.getItem("quiz.fat_level") ?? 0);
 const dirName = ref(null);
 const ageDir = ref(null);
 onActivated(() => {
-    dirName.value = sessionStorage.getItem("quiz.sex");
+  dirName.value = sessionStorage.getItem("quiz.sex");
 
-	const bday = sessionStorage.getItem("quiz.bday");
-    let age = calculateAge(bday);
+  const bday = sessionStorage.getItem("quiz.bday");
+  let age = calculateAge(bday);
 
-    ageDir.value =
-        age < 30
-            ? "18"
-            : age < 40
-            ? "30"
-            : age < 50
-            ? "40"
-            : "50";
+  ageDir.value = age < 30 ? "18" : age < 40 ? "30" : age < 50 ? "40" : "50";
 });
 
 const fileName = computed(() => {
-    return `${+level.value + 1}.webm`;
+  return `${+level.value + 1}.webm`;
 });
 
 function calculateAge(birthDate) {
-    const birthDateParts = birthDate.split("-");
-    const birthYear = parseInt(birthDateParts);
-    const birthMonth = parseInt(birthDateParts);
-    const birthDay = parseInt(birthDateParts);
+  const birthDateParts = birthDate.split("-");
+  const birthYear = parseInt(birthDateParts);
+  const birthMonth = parseInt(birthDateParts);
+  const birthDay = parseInt(birthDateParts);
 
-    const today = new Date();
-    const currentYear = today.getFullYear();
-    const currentMonth = today.getMonth() + 1;
-    const currentDay = today.getDate();
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  const currentMonth = today.getMonth() + 1;
+  const currentDay = today.getDate();
 
-    let age = currentYear - birthYear;
+  let age = currentYear - birthYear;
 
-    if (
-        currentMonth < birthMonth ||
-        (currentMonth === birthMonth && currentDay < birthDay)
-    ) {
-        age--;
-    }
+  if (
+    currentMonth < birthMonth ||
+    (currentMonth === birthMonth && currentDay < birthDay)
+  ) {
+    age--;
+  }
 
-    return age;
+  return age;
 }
 
 function tryNext() {
-    sessionStorage.setItem("quiz.fat_level", level.value);
-    nextPage();
+  sessionStorage.setItem("quiz.fat_level", level.value);
+  nextPage();
 }
 </script>
 
 <template>
-    <main class="page">
-        <section class="quiz section-padding">
-            <div class="quiz__container-little">
-                <ProgressBar />
-                <div class="quiz-content _fat-goal">
-                    <h2 class="quiz-content__title-mobile _hidden title">
-                        Выберите свой уровень жира в&nbsp;организме
-                    </h2>
-                    <div class="quiz-content__img-wrapper" style="height: 680px; display: flex; justify-content: center">
-						<video
-							autoplay
-							muted
-							loop
-							:src="`/video/${dirName}/${ageDir}/1.webm`"
-							alt="Image"
-							v-show="level === 0"
-							poster="/resources/images/video-poster.jpg"
-						/>
-						<video
-							autoplay
-							muted
-							loop
-							:src="`/video/${dirName}/${ageDir}/2.webm`"
-							alt="Image"
-							v-show="level === 1"
-							poster="/resources/images/video-poster.jpg"
-						/>
-						<video
-							autoplay
-							muted
-							loop
-							:src="`/video/${dirName}/${ageDir}/3.webm`"
-							alt="Image"
-							v-show="level === 2"
-							poster="/resources/images/video-poster.jpg"
-						/>
-						<video
-							autoplay
-							muted
-							loop
-							:src="`/video/${dirName}/${ageDir}/4.webm`"
-							alt="Image"
-							v-show="level === 3"
-							poster="/resources/images/video-poster.jpg"
-						/>
-						<video
-							autoplay
-							muted
-							loop
-							:src="`/video/${dirName}/${ageDir}/5.webm`"
-							alt="Image"
-							v-show="level === 4"
-							poster="/resources/images/video-poster.jpg"
-						/>
-						<video
-							autoplay
-							muted
-							loop
-							:src="`/video/${dirName}/${ageDir}/6.webm`"
-							alt="Image"
-							v-show="level === 5"
-							poster="/resources/images/video-poster.jpg"
-						/>
-						<video
-							autoplay
-							muted
-							loop
-							:src="`/video/${dirName}/${ageDir}/7.webm`"
-							alt="Image"
-							v-show="level === 6"
-							poster="/resources/images/video-poster.jpg"
-						/>
-                    </div>
-                    <div class="quiz-content__info">
-                        <h2 class="quiz-content__title _hidden title">
-                            Выберите свой уровень жира в&nbsp;организме
-                        </h2>
-                        <div class="quiz-content__range">
-                            <Slider
-                                :min="0"
-                                :max="6"
-                                :step="1"
-                                :tooltips="false"
-                                from="5-9%"
-                                to="&gt;40%"
-                                @change="(e) => (level = e)"
-                            />
-                        </div>
-                        <div class="quiz-content__action">
-                            <NextButton @click="tryNext" />
-                        </div>
-                    </div>
-                </div>
+  <main class="page">
+    <section class="quiz section-padding">
+      <div class="quiz__container-little">
+        <ProgressBar />
+        <div class="quiz-content _fat-goal">
+          <h2 class="quiz-content__title-mobile _hidden title">
+            Выберите свой уровень жира в&nbsp;организме
+          </h2>
+          <div
+            class="quiz-content__img-wrapper"
+            style="height: 680px; display: flex; justify-content: center"
+          >
+            <video
+              autoplay
+              muted
+              loop
+              disablepictureinpicture=""
+              webkit-playsinline=""
+              playsinline=""
+              pip="false"
+              :src="`/video/${dirName}/${ageDir}/${level + 1}.webm`"
+              alt="Image"
+              poster="/resources/images/video-poster.jpg"
+            />
+          </div>
+          <div class="quiz-content__info">
+            <h2 class="quiz-content__title _hidden title">
+              Выберите свой уровень жира в&nbsp;организме
+            </h2>
+            <div class="quiz-content__range">
+              <Slider
+                :min="0"
+                :max="6"
+                :step="1"
+                :tooltips="false"
+                from="5-9%"
+                to="&gt;40%"
+                @change="(e) => (level = e)"
+              />
             </div>
-        </section>
-    </main>
+            <div class="quiz-content__action">
+              <NextButton @click="tryNext" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
+
+<style scoped>
+video {
+  background-color: transparent;
+  border-bottom: 1px solid transparent; /* хак для предотвращения мелькания при загрузке на некоторых версиях ios */
+  &::-webkit-media-controls-panel {
+    display: none !important;
+    -webkit-appearance: none;
+    opacity: 0;
+    visibility: hidden;
+  }
+  &::-webkit-media-controls-play-button {
+    display: none !important;
+    -webkit-appearance: none;
+    opacity: 0;
+    visibility: hidden;
+  }
+  &::-webkit-media-controls-start-playback-button {
+    display: none !important;
+    -webkit-appearance: none;
+    opacity: 0;
+    visibility: hidden;
+  }
+}
+</style>
